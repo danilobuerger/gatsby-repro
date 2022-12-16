b0ee0c45f6a73fcce1bdf873453855fc88205df3
Danilo Bürger <danilo@feastr.de>
2022-12-16 12:38:22 +0100

    Added gatsby-plugin-image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const pageStyles = {
   color: "#232129",
@@ -139,6 +140,7 @@ const links = [
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
+      {/* <GatsbyImage alt="test" /> */}
       <h1 style={headingStyles}>
         Congratulations
         <br />
